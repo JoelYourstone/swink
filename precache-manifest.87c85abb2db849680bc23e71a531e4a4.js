c3899c5fb4dde836939fbcc847bd66611835b14a
Daniel Odenbrand <wishing@live.se>
2020-01-06 12:37:46 +0100

    Updates

---
+++ precache-manifest.87c85abb2db849680bc23e71a531e4a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddaba800df3d8d5b5fa1d58e1d58d8f1",
+    "url": "/swink/index.html"
+  },
+  {
+    "revision": "580e61e7c15e0dbb8c1f",
+    "url": "/swink/static/css/2.797a6756.chunk.css"
+  },
+  {
+    "revision": "b5a08543ffe6262e03b7",
+    "url": "/swink/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "580e61e7c15e0dbb8c1f",
+    "url": "/swink/static/js/2.9248581c.chunk.js"
+  },
+  {
+    "revision": "b5a08543ffe6262e03b7",
+    "url": "/swink/static/js/main.7a9fd3b7.chunk.js"
+  },
+  {
+    "revision": "c9968ea6d7b7ca13d4b4",
+    "url": "/swink/static/js/runtime~main.389201ce.js"
+  }
+]);
